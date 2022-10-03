c62956e48f4e25ccf88b835ed60ed47bc6e00168
Enzo Marchesi <80432840+lemarchesi09@users.noreply.github.com>
2022-10-03 17:07:26 -0300

    Estructura completa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import "./app.css";
+import { Routes, Route } from 'react-router-dom';
+import { Dashboard } from "./components/Dashboard";
+import { Details } from "./components/Details";
+import { Home } from "./components/Home";
+import { NavBar } from "./components/NavBar";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        {/*Agregar enrutado dinamico con uso de id*/}
+        <Route path="/details" element={<Details />}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
