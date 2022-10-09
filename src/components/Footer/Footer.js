aece2c62138e5e80d7b5727b158307a9144a668a
Camila Stefano <camilavictoriastefano@gmail.com>
2022-10-08 21:02:26 -0300

    feat: files reorganized

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import "./footer.css";
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <div className="foot-cont">
+      <div className="foot-text">
+        <a href="https://github.com/SebaDAlessandro">Sebastian D.</a>
+        <a href="https://github.com/lemarchesi09">Enzo M.</a>
+        <a href="https://github.com/CamiStefano">Camila S.</a>
+        <a href="https://www.avalith.net/" className="avalith__link" target="_blank" rel="noreferrer">
+          <p className="avalith">
+            AVALITH<span className="punto">.</span>
+          </p>
+        </a>
+      </div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './app/store';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Styles
+import "./styles/variables.css";
+import "./styles/styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<BrowserRouter>
+  <BrowserRouter>
     <Provider store={store}>
-        <App /> 
+      <App />
     </Provider>
-</BrowserRouter>
-
+  </BrowserRouter>
 );
-
-
