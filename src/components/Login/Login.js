d8aa121e365469d17248d5ff277f07a926adf42c
Enzo Marchesi <80432840+lemarchesi09@users.noreply.github.com>
2022-10-11 17:23:53 -0300

    :bug: Fixed image in Dashboard - :construction: Added Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,15 +6,15 @@ import { useState } from "react";
 
 export const Login = () =>{
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const [login, setLogin] = useState([
-        {
-            user: "",
-            password: ""
-        }
-    ])
-    const onSubmit = data => console.log(data);
+    // const [login, setLogin] = useState([{}])
 
-    console.log(watch("example")); // watch input value by passing the name of it
+    const onSubmit = (data, e) => {
+        const fields= {
+                user: data.user,
+                password: data.password
+            }
+        console.log(data);
+    }
 
     return (
         /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
@@ -34,15 +34,34 @@ export const Login = () =>{
                 value: 20,
                 message: "Field must be less than 20 characters"
             },
-            pattern: {
-                
-            }
+
         })} />
         
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
         {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+        {errors.user && <span>{errors.user.message}</span>}
+
+        <input
+        id="password"
+        type="password"
+        placeholder="Insert Password"
+        autoComplete="off"
+        {...register("password", {
+            required: {
+                value: true,
+                message: "Field required"
+            },
+            maxLength: {
+                value: 20,
+                message: "Field must be less than 20 characters"
+            },
+            minLength: {
+                value: 6,
+                message: "Field must contain more than 6 characters"
+            },
+
+        })} />
+        {errors.password && <span>{errors.password.message}</span>}
+ 
         
         <input type="submit" />
         </form>
